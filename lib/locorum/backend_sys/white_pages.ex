cd406d4cda949ead443b4f785b4b8778c446feda
Dave Lively <davelively14@gmail.com>
2016-04-03 13:34:31 -0400

    Reorged, ready for integration with BackendSys

--- lib/locorum/backend_sys/white_pages.ex
+++ lib/locorum/backend_sys/white_pages.ex
@@ -6,7 +6,7 @@ defmodule Locorum.BackendSys.WhitePages do
     Task.start_link(__MODULE__, :fetch, [query, query_ref, owner, limit])
   end
 
-  def fetch(query, _query_ref, _owner, _limit) do
+  def fetch(query, query_ref, owner, _limit) do
     city = query.city
     state = query.state
     biz =
@@ -16,12 +16,14 @@ defmodule Locorum.BackendSys.WhitePages do
 
     pull_data(city, state, biz)
     |> parse_data()
+    |> send_results(query_ref, owner)
   end
 
   defp pull_data(city, state, biz) do
     case HTTPoison.get(get_url(city, state, biz)) do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
         body
+      # TODO: Determine if we need these error reports or not? Maybe just log?
       {:ok, %HTTPoison.Response{status_code: 404}} ->
         {:error, "404"}
       {:ok, %HTTPoison.Response{status_code: 403}} ->
@@ -31,24 +33,31 @@ defmodule Locorum.BackendSys.WhitePages do
     end
   end
 
-  defp get_url(city, state, biz) do
-    "http://www.whitepages.com/business/" <> String.upcase(state) <> "/" <> city <> "/" <> biz
-  end
-
   def parse_data(body) do
     name = parse_item(Floki.find(body, "p[itemprop=name]"))
     address = parse_item(Floki.find(body, "span[itemprop=streetAddress]"))
     city = parse_item(Floki.find(body, "span[itemprop=addressLocality]"))
     state = parse_item(Floki.find(body, "span[itemprop=addressRegion]"))
     zip = parse_item(Floki.find(body, "span[itemprop=postalCode]"))
-    %Search{biz: name, address1: address, city: city, state: state, zip: zip }
+
+    add_to_search(List.zip([name, address, city, state, zip]))
   end
 
-  def parse_item([]), do: []
-  def parse_item([{_,[{_,_}],[item]} | tail]), do: [item | parse_item(tail)]
-  def parse_item([{_,[{_,_},{_,_}],[item]} | tail]), do: [item | parse_item(tail)]
+  defp get_url(city, state, biz) do
+    "http://www.whitepages.com/business/" <> String.upcase(state) <> "/" <> city <> "/" <> biz
+  end
 
-  def test_pull() do
-    pull_data("atlanta", "GA", "lucas-group")
+  defp send_results(nil, query_ref, owner), do: send(owner, {:results, query_ref, []})
+  defp send_results(results, query_ref, owner) do
+    send(owner, {:results, query_ref, results})
+  end
+
+  defp parse_item([]), do: []
+  defp parse_item([{_,[{_,_}],[item]} | tail]), do: [item | parse_item(tail)]
+  defp parse_item([{_,[{_,_},{_,_}],[item]} | tail]), do: [item | parse_item(tail)]
+
+  defp add_to_search([]), do: []
+  defp add_to_search([{name, address, city, state, zip} | tail]) do
+    [[%Search{biz: name, address1: address, city: city, state: state, zip: zip } | add_to_search(tail)]]
   end
 end
