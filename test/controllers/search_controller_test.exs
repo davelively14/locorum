c2b81e00592b18ea9ba836a66c0565cfe4769d8a
Dave Lively <davelively14@gmail.com>
2016-04-07 14:16:00 -0400

    Added a few tests

--- test/controllers/search_controller_test.exs
+++ test/controllers/search_controller_test.exs
@@ -2,10 +2,19 @@ defmodule Locorum.SearchControllerTest do
   use Locorum.ConnCase
   alias Locorum.Search
 
-  @valid_attributes %{biz: "A Biz Name", zip: "34593", city: "Atlanta", state: "GA"}
-  @invalid_attributes %{zip: "1234"}
+  @valid_attrs %{biz: "A Biz Name", zip: "34593", city: "Atlanta", state: "GA"}
+  @invalid_attrs %{zip: "1234"}
 
-  test "creates search and redirects" do
-    
+  test "creates search and redirects to results page", %{conn: conn} do
+    conn = post conn, search_path(conn, :create), search: @valid_attrs
+    search = Repo.get_by!(Search, @valid_attrs)
+    assert redirected_to(conn) == results_path(conn, :show, search)
+  end
+
+  test "deletes search and redirects to index page", %{conn: conn} do
+    search = Repo.insert! %Search{}
+    conn = delete conn, search_path(conn, :delete, search)
+    assert redirected_to(conn) == search_path(conn, :index)
+    refute Repo.get(Search, search.id)
   end
 end
