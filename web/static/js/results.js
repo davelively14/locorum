7eccdf4bdbf830eb1e775ce22e44179719280550
Dave Lively <davelively14@gmail.com>
2016-04-12 08:20:07 -0400

    Simple results.js built

--- web/static/js/results.js
+++ web/static/js/results.js
@@ -1,13 +1,25 @@
 let Results = {
-  init(){
-    let resName = document.getElementById("res-biz")
-    let postButton = document.getElementById("test-fill")
 
-    postButton.addEventListener("click", e => {
-      e.preventDefault()
-      let template = document.createElement("span")
-      resName.innerHTML = "<b>Lucas Group</b>"
-    })
+  init(socket, searchId){
+    socket.connect()
+    this.onReady(searchId, socket)
+  },
+
+  onReady(searchId, socket){
+    let resultContainer = document.getElementById("result-container")
+    let searchChannel   = socket.channel("searches:" + searchId)
+    // TODO join the search channel
   }
 }
 export default Results
+
+// init(){
+//   let resName = document.getElementById("res-biz")
+//   let postButton = document.getElementById("test-fill")
+//
+//   postButton.addEventListener("click", e => {
+//     e.preventDefault()
+//     let template = document.createElement("span")
+//     resName.innerHTML = "<b>Lucas Group</b>"
+//   })
+// }
