8c9916221461ede2ca225d94d031813ca4ce8c79
Dave Lively <davelively14@gmail.com>
2016-04-21 16:57:12 -0400

    Some HTTP parsing for Bing. Not working yet.

--- lib/locorum/backend_sys/bing.ex
+++ lib/locorum/backend_sys/bing.ex
@@ -9,9 +9,10 @@ defmodule Locorum.BackendSys.Bing do
     Task.start_link(__MODULE__, :fetch, [query, query_ref, owner, limit])
   end
 
-  def fetch(query, _query_ref, owner, _limit) do
+  def fetch(query, _query_ref, _owner, _limit) do
     get_url(query)
     |> Helpers.fetch_html
+    |> parse_data
   end
 
   def get_url(query) do
@@ -26,5 +27,45 @@ defmodule Locorum.BackendSys.Bing do
     "http://www.bing.com/search?q=#{biz}+#{city}+#{state}"
   end
 
-  defp key_id, do: Application.get_env(:locorum, :bing)[:key]
+  def parse_data(body) do
+    results =
+      Floki.find(body, "div.ent_cnt")
+      |> Floki.raw_html
+      |> Floki.find("span.b_address")
+      |> parse_item
+      # |> add_to_results
+
+    results
+  end
+
+  defp parse_item([]), do: []
+  defp parse_item([{_, _,[item]} | tail]), do: [String.strip(item) | parse_item(tail)]
+  defp parse_item([item | tail]), do: [String.strip(item) | parse_item(tail)]
+
+  # defp add_to_results(list) do
+  #   temp_list = List.reverse(list)
+  #
+  #   state_zip =
+  #     temp_list
+  #     |> Enum.at(0)
+  #     |> String.split(" ")
+  #
+  #   state = Enum.at(state_zip, 0)
+  #   zip = Enum.at(state_zip, 1)
+  #   city = Enum.at(temp_list, 1)
+  #   address =
+  #     temp_list
+  #     |> pop_tops(2)
+  #     |> rebuild_address
+  #
+  #
+  # end
+  #
+  # def pop_tops([head|tail], current) when current > 0, do: pop_tops(tail, current - 1)
+  # def pop_tops(remaining, current), do: remaining
+  #
+  # def rebuild_address([]), do: ""
+  # def rebuild_address([head|tail]), do: rebuild_address(tail, head)
+  # def rebuild_address([], result), do: result
+  # def rebuild_address([head|tail], result), do: rebuild_address(tail, "#{result}, #{head}")
 end
