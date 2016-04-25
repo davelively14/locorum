004148d2284a9b83e3f42c014146ad7b06514089
Dave Lively <davelively14@gmail.com>
2016-04-25 10:59:34 -0400

    Configured production for heroku

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -5,7 +5,8 @@ defmodule Locorum.UserSocket do
   channel "searches:*", Locorum.SearchChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+    timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
