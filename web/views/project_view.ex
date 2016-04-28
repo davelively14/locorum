a7676c75922566dfa044797c87c95160a1a74061
Dave Lively <davelively14@gmail.com>
2016-04-28 10:31:18 -0400

    Added functions to ProjectView

--- web/views/project_view.ex
+++ web/views/project_view.ex
@@ -1,10 +1,21 @@
 defmodule Locorum.ProjectView do
   use Locorum.Web, :view
+  alias Locorum.User
+  alias Locorum.Repo
 
   def get_username(id) do
-    user =
-      Locorum.User
-      |> Locorum.Repo.get(id)
+    user = get_user(id)
     user.username
+  end
+
+  def get_name(id) do
+    user = get_user(id)
+    user.name
+  end
+
+  def get_user(id) do
+    user =
+      User
+      |> Repo.get(id)
   end
 end
