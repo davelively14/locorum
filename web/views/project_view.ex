ab0231eef293b9db9104862212747d708d67a823
Dave Lively <davelively14@gmail.com>
2016-04-28 10:32:59 -0400

    Reduced to get_user/1 function

--- web/views/project_view.ex
+++ web/views/project_view.ex
@@ -3,16 +3,6 @@ defmodule Locorum.ProjectView do
   alias Locorum.User
   alias Locorum.Repo
 
-  def get_username(id) do
-    user = get_user(id)
-    user.username
-  end
-
-  def get_name(id) do
-    user = get_user(id)
-    user.name
-  end
-
   def get_user(id) do
     user =
       User
