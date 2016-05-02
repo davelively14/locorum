8208bbcba99423d3106bb1aabcc99d9b5191bb0c
Dave Lively <davelively14@gmail.com>
2016-05-02 15:29:04 -0400

    Have a solution for CSV...just need to get File.read! to work

--- web/controllers/csv_controller.ex
+++ web/controllers/csv_controller.ex
@@ -1,7 +1,7 @@
 defmodule Locorum.CSVController do
   use Locorum.Web, :controller
 
-  def create(conn, %{"csv" => csv}) do
+  def create(_conn, %{"csv" => csv}) do
     case File.read!(csv) do
       {:ok, result} ->
         result =
@@ -9,7 +9,7 @@ defmodule Locorum.CSVController do
           |> String.split("\n")
           |> Locorum.BackendSys.Helpers.pop_first(1)
           |> split_results
-        # TODO escaping quotations ruins everthing. Hardcode acceptance.
+          |> add_to_search
       _ ->
         {:error, "Invalid file type"}
     end
@@ -18,9 +18,7 @@ defmodule Locorum.CSVController do
   def split_results([]), do: []
   def split_results([head|tail]), do: [String.split(head, ",")|split_results(tail)]
 
-  def join_results(keys, values) do
-    join_results(keys, values, %{})
-  end
+  def join_results(keys, values), do: join_results(keys, values, %{})
   def join_results([], [], acc), do: acc
   def join_results([], _, acc), do: {:error, "Values longer than keys"}
   def join_results(_, [], acc), do: {:error, "Keys longer than values"}
@@ -32,8 +30,20 @@ defmodule Locorum.CSVController do
   def get_size([], acc), do: acc
   def get_size([head|tail], acc), do: get_size(tail, acc + 1)
 
-  # def add_to_search([]), do: []
-  # def add_to_search([head|tail]) do
-  #
-  # end
+  def add_to_search([]), do: []
+  def add_to_search([head|tail]) do
+    biz = Enum.at(head, 1)
+    address1 = Enum.at(head, 2)
+    address2 =
+      case Enum.at(head, 3) do
+        "" -> nil
+        result -> result
+      end
+    city = Enum.at(head, 4)
+    state = Enum.at(head, 6)
+    zip = Enum.at(head, 8)
+    phone = Enum.at(head, 9)
+    [%Locorum.Search{biz: biz, address1: address1, address2: address2, city: city,
+                     state: state, zip: zip, phone: phone}]
+  end
 end
