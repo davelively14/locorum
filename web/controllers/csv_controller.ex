9bd2bd652524d1371390b1da2a9836f5bb3131c4
Dave Lively <davelively14@gmail.com>
2016-05-02 20:01:37 -0400

    Passes project_id and user_id

--- web/controllers/csv_controller.ex
+++ web/controllers/csv_controller.ex
@@ -1,7 +1,7 @@
 defmodule Locorum.CSVController do
   use Locorum.Web, :controller
 
-  def create(conn, %{"search" => %{"csv" => csv}}) do
+  def create(conn, %{"search" => %{"csv" => csv, "project_id" => project_id, "user_id" => user_id}}) do
     searches =
       case File.read(csv.path) do
         {:ok, result} ->
@@ -10,7 +10,7 @@ defmodule Locorum.CSVController do
             |> String.split("\n")
             |> Locorum.BackendSys.Helpers.pop_first(1)
             |> split_results
-            |> add_to_search
+            |> add_to_search([project_id, user_id])
         _ ->
           {:error, "Invalid file type"}
       end
@@ -20,20 +20,20 @@ defmodule Locorum.CSVController do
   def split_results([]), do: []
   def split_results([head|tail]), do: [String.split(head, ",")|split_results(tail)]
 
-  def join_results(keys, values), do: join_results(keys, values, %{})
-  def join_results([], [], acc), do: acc
-  def join_results([], _, _acc), do: {:error, "Values longer than keys"}
-  def join_results(_, [], _acc), do: {:error, "Keys longer than values"}
-  def join_results([keys_head|keys_tail], [values_head|values_tail], acc) do
-    join_results(keys_tail, values_tail, Map.put(acc, keys_head, values_head))
-  end
+  # def join_results(keys, values), do: join_results(keys, values, %{})
+  # def join_results([], [], acc), do: acc
+  # def join_results([], _, _acc), do: {:error, "Values longer than keys"}
+  # def join_results(_, [], _acc), do: {:error, "Keys longer than values"}
+  # def join_results([keys_head|keys_tail], [values_head|values_tail], acc) do
+  #   join_results(keys_tail, values_tail, Map.put(acc, keys_head, values_head))
+  # end
+  #
+  # def get_size(list), do: get_size(list, 0)
+  # def get_size([], acc), do: acc
+  # def get_size([_head|tail], acc), do: get_size(tail, acc + 1)
 
-  def get_size(list), do: get_size(list, 0)
-  def get_size([], acc), do: acc
-  def get_size([_head|tail], acc), do: get_size(tail, acc + 1)
-
-  def add_to_search([]), do: []
-  def add_to_search([head|tail]) do
+  def add_to_search([], _), do: []
+  def add_to_search([head|tail], [project_id, user_id]) do
     biz = Enum.at(head, 1)
     address1 =
       case Enum.at(head, 3) do
@@ -45,6 +45,7 @@ defmodule Locorum.CSVController do
     zip = Enum.at(head, 8)
     phone = Enum.at(head, 9)
     [%Locorum.Search{biz: biz, address1: address1, city: city, state: state,
-                     zip: zip, phone: phone}|add_to_search(tail)]
+                     zip: zip, phone: phone, project_id: project_id,
+                     user_id: user_id}|add_to_search(tail, [project_id, user_id])]
   end
 end
