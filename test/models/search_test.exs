5c84e8990bfe65d3e6d3557fef33aef11783df4a
Dave Lively <davelively14@gmail.com>
2016-06-06 18:27:42 -0400

    Added user authenticiation test

--- test/models/search_test.exs
+++ test/models/search_test.exs
@@ -6,6 +6,21 @@ defmodule Locorum.SearchTest do
   @valid_attrs %{biz: "A Biz Name", zip: "34593", city: "Atlanta", state: "GA", address1: "369 James St SE", phone: "4042607121"}
   @invalid_attrs %{zip: "1234"}
 
+  test "requires user authenticiation for all search actions", %{conn: conn} do
+    Enum.each([
+      get(conn, search_path(conn, :new)),
+      get(conn, search_path(conn, :index)),
+      get(conn, search_path(conn, :show, "1")),
+      get(conn, search_path(conn, :edit, "1")),
+      put(conn, search_path(conn, :update, "1", %{})),
+      post(conn, search_path(conn, :create, %{})),
+      delete(conn, search_path(conn, :delete, "1"))
+    ], fn conn ->
+      assert html_response(conn, 302)
+      assert conn.halted
+    end)
+  end
+
   test "creates search and redirects to results page", %{conn: conn} do
     conn = post conn, search_path(conn, :create), search: @valid_attrs
     search = Repo.get_by!(Search, @valid_attrs)
