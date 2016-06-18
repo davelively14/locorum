e1dcf419bd34d94040c5d67bfe9bcd6bea9af699
Dave Lively <davelively14@gmail.com>
2016-06-18 09:19:17 -0400

    Can parse name from Yelp

--- lib/locorum/backend_sys/yelp.ex
+++ lib/locorum/backend_sys/yelp.ex
@@ -9,13 +9,50 @@ defmodule Locorum.BackendSys.Yelp do
   def fetch(query, _query_ref, _owner, _limit) do
     query
     |> get_url
+    |> Helpers.fetch_html
+    |> parse_data
   end
 
   def get_url(query) do
-    city = query.city |> Helpers.convert_to_utf("+")
-    state = query.state
+    address = query.address1 |> Helpers.convert_to_utf("+")
     name = query.biz |> Helpers.convert_to_utf("+")
 
-    "https://www.yelp.com/search?find_desc=#{name}&find_loc=#{city}+#{state}"
+    "https://www.yelp.com/search?find_desc=#{name}&find_loc=#{address}"
   end
+
+  def parse_data(body) do
+    focus = body |> Floki.find(".regular-search-result")
+
+    name =
+      focus
+      |> Enum.map(&(Floki.find(&1, ".biz-name") |> List.first |> elem(2) |> List.first |> elem(2) |> get_names))
+
+    address_group =
+      focus
+      |> Floki.find("address")
+      |> Floki.text
+      |> String.strip
+      |> String.split("\n")
+
+    phone =
+      focus
+      |> Floki.find(".biz-phone")
+      |> Floki.text
+      |> String.strip
+  end
+
+  def get_names(element) do
+    element
+    |> strip_format
+    |> list_to_spaced_str
+  end
+
+  def strip_format([]), do: []
+  def strip_format([head|tail]) when is_tuple(head), do: [Floki.text(head) | strip_format(tail)]
+  def strip_format([head|tail]), do: [String.strip(head) | strip_format(tail)]
+
+  def list_to_spaced_str([]), do: nil
+  def list_to_spaced_str([head|tail]), do: list_to_spaced_str(tail, "#{head}")
+  def list_to_spaced_str([], str), do: str
+  def list_to_spaced_str([head|tail], str), do: list_to_spaced_str(tail, "#{str} #{head}")
 end
