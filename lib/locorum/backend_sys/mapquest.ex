5f3ed240bd54b618ce66c8c8d0afdb8a54fef99d
Dave Lively <davelively14@gmail.com>
2016-06-20 23:01:34 -0400

    Mapquest complete. Can't scrape phone number.

--- lib/locorum/backend_sys/mapquest.ex
+++ lib/locorum/backend_sys/mapquest.ex
@@ -1,16 +1,17 @@
 defmodule Locorum.BackendSys.Mapquest do
   alias Locorum.BackendSys.Helpers
-  # alias Locorum.BackendSys.Result
+  alias Locorum.BackendSys.Result
 
   def start_link(query, query_ref, owner, limit) do
     Task.start_link(__MODULE__, :fetch, [query, query_ref, owner, limit])
   end
 
-  def fetch(query, _query_ref, _owner, _limit) do
+  def fetch(query, _query_ref, owner, _limit) do
     query
     |> get_url
-    |> Helpers.fetch_html
+    |> Helpers.init_html(__MODULE__, owner, query)
     |> parse_data
+    |> Helpers.send_results(__MODULE__, owner, query)
   end
 
   def get_url(query) do
@@ -22,7 +23,44 @@ defmodule Locorum.BackendSys.Mapquest do
   end
 
   def parse_data(body) do
-    focus = body |> Floki.find("script[id=SearchResults]") |> List.first |> elem(2) |> List.first |> Poison.decode!
-    focus
+    focus =
+      body
+      |> Floki.find("script[id=SearchResults]")
+      |> List.first
+      |> elem(2)
+      |> List.first
+      |> Poison.decode!
+      |> Map.fetch!("results")
+      |> Enum.map(&(if !&1["@adparameters"], do: &1))
+      |> strip_nil
+
+    name = focus |> Enum.map(&(&1["name"]))
+
+    address = focus |> Enum.map(&(&1["address"]["address1"]))
+
+    city = focus |> Enum.map(&(&1["address"]["locality"]))
+
+    state = focus |> Enum.map(&(&1["address"]["region"]))
+
+    zip = focus |> Enum.map(&(&1["address"]["postalCode"]))
+
+    url = focus |> Enum.map(&("https://www.mapquest.com#{&1["slug"]}"))
+
+    add_to_result List.zip([name, address, city, state, zip, url])
+  end
+
+  def add_to_result([]), do: []
+  def add_to_result([{name, address, city, state, zip, url} | tail]) do
+    [%Result{biz: name, address: address, city: city, state: state, zip: zip, url: url, phone: "N/A"} | add_to_result(tail)]
+  end
+
+  def strip_nil([]), do: []
+  def strip_nil([head|tail]) do
+    case head do
+      nil ->
+        strip_nil(tail)
+      _ ->
+        [head | strip_nil(tail)]
+    end
   end
 end
