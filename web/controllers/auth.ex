68d17c9fa0b87a939d608114833efdaa27322144
Dave Lively <davelively14@gmail.com>
2016-07-10 20:21:49 -0400

    Added a TODO

--- web/controllers/auth.ex
+++ web/controllers/auth.ex
@@ -24,6 +24,7 @@ defmodule Locorum.Auth do
   def login(conn, user) do
     conn
     |> put_current_user(user)
+    # TODO do we need this assign? put_current_user does this same thing
     |> assign(:current_user, user)
     |> put_session(:user_id, user.id)
     |> configure_session(renew: true)
