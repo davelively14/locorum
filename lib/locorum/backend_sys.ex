343a7ea64cdd18bebe05384d248e269fb460444c
Dave Lively <davelively14@gmail.com>
2016-11-06 08:14:25 -0500

    Completed most todos, user_id now pulled from socket

--- lib/locorum/backend_sys.ex
+++ lib/locorum/backend_sys.ex
@@ -21,17 +21,11 @@ defmodule Locorum.BackendSys do
 
   # Determine the options, broadcast to channel to setup everything, then call
   # the Supervisor to run the backends.
-  # NOTE as we adjust broadcasts, ensure we do so in the JS, too
   def compute(query, socket, opts \\ []) do
 
     limit = opts[:limit] || 10
 
     backends = opts[:backends] || Repo.all(Backend) |> Enum.map(&(Map.get(&1, :module) |> String.to_atom))
-    # TODO remove this
-    # backends = [Locorum.BackendSys.Yelp, Locorum.BackendSys.Mapquest, Locorum.BackendSys.Neustar, Locorum.BackendSys.Google, Locorum.BackendSys.Facebook, Locorum.BackendSys.Yahoo, Locorum.BackendSys.Bing, Locorum.BackendSys.CityGrid]
-
-    # TODO delete. Start the app in mix.exs now
-    # HTTPoison.start
 
     changeset = ResultCollection.changeset(%ResultCollection{search_id: query.id}, %{search_id: query.id})
     result_collection_id =
@@ -44,19 +38,11 @@ defmodule Locorum.BackendSys do
 
     socket = assign(socket, :result_collection_id, result_collection_id)
     project_id = socket.assigns.project_id
-    # TODO fix user_id, just using a known temp right now
-    user_id = 1
+    user_id = socket.assigns.user_id
 
     backends |> Enum.each(&(Repo.get_by!(Backend, module: Atom.to_string(&1)) |> init_frontend(query, user_id, socket)))
 
-    # TODO remove
-    # Don't think we need spawn_query any more, we're just going to call the
-    # Supervise function for everything.
-    # backends
-    # |> Enum.map(&spawn_query(&1, query, socket, limit))
-    # {:ok, pid} =
-      Locorum.BackendSys.BackendsSupervisor.start_link(project_id, query, socket, backends)
-    # Process.monitor(pid)
+    Locorum.BackendSys.BackendsSupervisor.start_link(project_id, query, socket, backends)
   end
 
   # Added for_user to allow the client to identify if the broadcast is meant for
@@ -72,18 +58,4 @@ defmodule Locorum.BackendSys do
       search_id: query.id
     }
   end
-
-  # TODO So...what does this do? Originally, the Supervisor would just monitor this process for some reason
-  # Originally, Locorum.BackendSys.Supervisor would only supervise one type of
-  # worker (BackendSys module), which allowed us to use :simple_one_for_one IOT
-  # allow for dynamic creation of workers. This won't work for backends, but it
-  # does work for the BackendSys portion of our supervision.
-  # TODO delete this
-  # defp spawn_query(backend, query, socket, limit) do
-  #   query_ref = make_ref()
-  #   opts = [backend, query, query_ref, socket, limit]
-  #   {:ok, pid} = Supervisor.start_child(Locorum.BackendSys.Supervisor, opts)
-  #   monitor_ref = Process.monitor(pid)
-  #   {pid, monitor_ref, query_ref}
-  # end
 end
